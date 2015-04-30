82d991b020cf9c591019bf1d5104ed37d72f01fd
flbaue <florian.bauer@posteo.de>
2015-05-01 01:41:30 +0200

    added navigation helper

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,22 @@
+module ApplicationHelper
+
+  # Returns the full title on a per-page basis.
+  def full_title(page_title = '')
+    base_title = "KitchnKit"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
+  def set_active_tab(name, open_tab)
+    puts "#{name} == #{open_tab}"
+    if name == open_tab
+      "active"
+    else
+      ""
+    end
+  end
+
+end
