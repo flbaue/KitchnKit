d7088104ff27fb193720486567498f851af1dc1f
flbaue <florian.bauer@posteo.de>
2015-04-30 20:04:32 +0200

    added recipe_ingredient model

---
+++ db/migrate/20150430180402_create_recipe_ingredients.rb
@@ -0,0 +1,10 @@
+class CreateRecipeIngredients < ActiveRecord::Migration
+  def change
+    create_table :recipe_ingredients do |t|
+      t.float :amount
+      t.string :unit
+
+      t.timestamps null: false
+    end
+  end
+end
